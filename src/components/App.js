ef6886f274fc3a11b3b98a5358fe5d502fd4a3dc
Manuel <manuverrastro@gmail.com>
2022-07-21 10:37:26 -0300

    Clase 2

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <>
+    <NavBar />
+    <main>
+      <h2>Home</h2>
+    </main>
+    <footer>
+      <p>Footer</p>
+    </footer>
+    </>
+  );
+}
+
+export default App;
